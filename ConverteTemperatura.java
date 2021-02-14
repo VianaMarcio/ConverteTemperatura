daeee69fd36fa2ff570a432dfb2e1a5f56554c67
Márcio de Freitas Viana <defreitasvianamarcio@gmail.com>
2021-02-14 19:23:40 -0300

    Update ConverteTemperatura.java

--- ConverteTemperatura.java
+++ ConverteTemperatura.java
@@ -2,8 +2,8 @@ package ConverteTemperatura;
 
 public class ConverteTemperatura {
     public static void main(String[] args) {
-        final int diferenca = 32;
-        final double divisao =  5/9.0;
+        final int DIFERENCA = 32;
+        final double DIVISAO =  5/9.0;
         double temperaturaF = 190.0;
         double temperaturaC = (temperaturaF - diferenca) * divisao;
         System.out.print(temperaturaC);
